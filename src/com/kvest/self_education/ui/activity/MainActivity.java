7cc3144df6e3943fb7de1ceccd692223f9f69cee
Roman Bielokon <roman_slavyansk@mail.ru>
2013-12-17 15:20:02 +0200

    project creation

---
+++ src/com/kvest/self_education/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kvest.self_education.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.kvest.self_education.R;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
